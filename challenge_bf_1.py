1d36b0d930581fe0e3922f4dad1d21234d07e02a
Jerem <silva.jeremie93@gmail.com>
2023-07-08 00:27:43 +0200

    DEV

---
+++ challenge_bf_1.py
@@ -0,0 +1,50 @@
+"""Challenge 00
+Given integer `N`.
+Generate list of string with length exactly `N`. Each element of string is 
+lowercase alphabet (a-z).
+Save the list as file with each line contain only a string.
+
+    Test
+Given `N = 2`, should produce `[ aa, ab, ac, ... , zy, zz ]`
+
+    Remarks
+Brute force is an attempt to try all possibilities of states to crack 
+verification system. The attack is based on trial and error. The heart of 
+this attack is wordlist, a large amount of words that will be verified one by 
+one.
+Larger wordlist will raise the chance of success but would require longer 
+time. Therefore, creating a correct wordlist is essentially useful.
+One approach is to create all words from all possible characters on certain 
+length. This require large space to store the result.
+"""
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+n = 4
+
+
+def bruteforce(character_list: list, lenght: int, combinations: list, word=""):
+    if len(word) <= lenght:
+        for character in character_list:
+            bruteforce(character_list, lenght, combinations, word+character)
+        combinations.append(word)
+
+
+def combinations_formater(combinations: list):
+    combinations.remove("")
+    return sorted(combinations, key=lambda x: (len(x), x))
+
+
+def sum_of_powers(base, highest_power):
+    return sum(base**i for i in range(1, highest_power + 1))
+
+
+combinations = []
+bruteforce(alphabet, n, combinations)
+combinations = combinations_formater(combinations)
+
+
+print(combinations)
+print(len(combinations))
+print(sum_of_powers(len(alphabet), n))
