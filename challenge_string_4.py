fe56b5c4b2028a7526b3b25329cad645755dc018
Jerem <silva.jeremie93@gmail.com>
2023-07-21 19:36:01 +0200

    DEV

---
+++ challenge_string_4.py
@@ -0,0 +1,51 @@
+"""Challenge 04
+Given a long string (text) `S`.
+Find and extract all substring starts with `http://` or `https://`.
+The substring should have no whitespace.
+Optionally, you can validate the URL by checking if they are in `FQDN (Full Name Qualified Domain)` form.
+
+## Remarks
+Finding valid URL in text is useful for scraping. When processing a paragraph,
+there should be many valuable information for recon.
+Valid URLs are not only found on a page. It can also be found on assets such as JavaScript file.
+"""
+
+
+text: str = "https://www.google.com/https://github.com/https://pypi.org/http://docs.python.org/fr/3/library/unittest.html"
+# google github pypi python
+
+def search_url_in_text(text: str) -> list[str]:
+    urls_list = text.split("https://")
+    for urls in urls_list:
+        second_list = urls.split("https://")
+    return urls_list + second_list
+
+# TODO
+
+
+
+
+urls_list = search_url_in_text(text)
+
+print(urls_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
