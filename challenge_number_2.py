4e86b4831d54dc2b425123d97fcea1b8fdd3e95e
Jerem <silva.jeremie93@gmail.com>
2023-07-27 15:16:44 +0200

    DEV

--- challenge_number_2.py
+++ challenge_number_2.py
@@ -10,17 +10,37 @@ in Discrete Math especially some topics on Cryptography.
 """
 
 N: int = 120
+expected_result = "2^3 x 3 x 5"
 
 
-def factors_of_number(N: int) -> list[int]:
-    factors_list = []
-    for i in range(1, N):
-        if N % i == 0:
-            factors_list.append(i)
-    return factors_list
+def recursive_divider_decomposer(N: int, divider_dict: dict = {}) -> dict:
+    for divider in range(2, N+1):
+        if N % divider == 0:
+            try:
+                divider_dict[divider] += 1
+            except KeyError:
+                divider_dict[divider] = 1
+            recursive_divider_decomposer(int(N/divider), divider_dict)
+            break
+    return divider_dict
 
 
-print(factors_of_number(N))
+def formatting_dividers(divider_dict: dict) -> str:
+    result = ""
+    for divider, iterations in divider_dict.items():
+        result += f"{divider}^{iterations} x "
+    return result[:-3]
 
 
-# TODO: fonction recursive pour chaque nombre, dans une boucle
+def check_dividers_result(divider_dict: dict) -> int:
+    result = 1
+    for divider, iterations in divider_dict.items():
+        result *= (divider ** iterations)
+    return result
+
+
+divider_dict: dict = recursive_divider_decomposer(N)
+
+print(divider_dict)
+print(formatting_dividers(divider_dict))
+print(check_dividers_result(divider_dict))
