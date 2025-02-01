fb25663a53c70a42fcc9c6750ebb8f491031cd82
silvajeremie <jeremie.silva@ze-watt.com>
2025-02-01 20:31:50 +0100

    new challenges

--- challenge_30.py
+++ challenge_30.py
@@ -4,16 +4,20 @@
 path: str = "/Users/Thibh/Desktop/Dossier_01/Tutoriel/Udemy"
 
 
-def move_up_by_loop(path: str) -> str:
-    pass
+def move_up_by_loop(path: str, up: int = 1) -> str:
+    return ("/").join(path.split("/")[:-up])
 
 
-def move_up_recursively(path: str) -> str:
-    pass
+def move_up_recursively(path: str, up: int = 1) -> str:
+    if up == 0:
+        return path
+    new_path: str = ("/").join(path.split("/")[:-1])
+    return move_up_recursively(path=new_path, up=up-1)
 
 
 if __name__ == "__main__":
-    results: str = move_up_by_loop(path)
-    print(results)
-    results: str = move_up_recursively(path)
-    print(results)
+    print(path)
+    first_results: str = move_up_by_loop(path=path, up=3)
+    print(first_results)
+    second_results: str = move_up_recursively(path=path, up=3)
+    print(second_results)
