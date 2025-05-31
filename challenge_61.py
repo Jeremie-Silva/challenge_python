29148223f33ea611fef5132fcbb9928d1ef9c0df
silvajeremie <jeremie.silva@ze-watt.com>
2025-05-31 15:19:16 +0200

    new challenges

--- challenge_61.py
+++ challenge_61.py
@@ -3,17 +3,22 @@
 
 class CustomList(list):
     def __init__(self, *args):
-        super().__init__(arg.upper() for arg in args if isinstance(arg, str))
+        super().__init__(arg for arg in set(args) if isinstance(arg, str))
 
     def append(self, value):
         if not isinstance(value, int):
+            if value in self:
+                return "valeur déjà présente"
             super().append(value)
         return "Nombres entiers interdits"
 
 
 
 if __name__ == "__main__":
-    custom_list: CustomList = CustomList("Pierre", 12345, "Marie", "Joseph")
+    custom_list: CustomList = CustomList("Pierre", "Pierre", 12345, "Marie", "Joseph")
     print(f"{custom_list=}")
+    print()
     print(custom_list.append(12345))
+    print(custom_list.append("Pierre"))
+    print()
     print(f"{custom_list=}")
